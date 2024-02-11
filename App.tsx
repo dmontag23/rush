eebec086f0696f134dc31322aa3878ec045cce4e
Dillon Montag <dillon@thoughtmachine.net>
2024-02-11 20:57:56 +0000

    Install react native paper

--- App.tsx
+++ App.tsx
@@ -17,6 +17,7 @@ import {
   useColorScheme,
   View
 } from 'react-native';
+import {PaperProvider} from 'react-native-paper';
 
 import {
   Colors,
@@ -65,36 +66,40 @@ function App(): React.JSX.Element {
 
   return (
     <NavigationContainer>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white
-            }}>
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <Section title="Learn More">
-              Read the docs to discover what to do next:
-            </Section>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      {/* PaperProvider should be the innermost provider for the app.
+      See https://callstack.github.io/react-native-paper/docs/guides/getting-started */}
+      <PaperProvider>
+        <SafeAreaView style={backgroundStyle}>
+          <StatusBar
+            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            backgroundColor={backgroundStyle.backgroundColor}
+          />
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={backgroundStyle}>
+            <Header />
+            <View
+              style={{
+                backgroundColor: isDarkMode ? Colors.black : Colors.white
+              }}>
+              <Section title="Step One">
+                Edit <Text style={styles.highlight}>App.tsx</Text> to change
+                this screen and then come back to see your edits.
+              </Section>
+              <Section title="See Your Changes">
+                <ReloadInstructions />
+              </Section>
+              <Section title="Debug">
+                <DebugInstructions />
+              </Section>
+              <Section title="Learn More">
+                Read the docs to discover what to do next:
+              </Section>
+              <LearnMoreLinks />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </PaperProvider>
     </NavigationContainer>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset']
+  presets: ['module:@react-native/babel-preset'],
+  /** The following is needed for react native paper.
+   *  See https://callstack.github.io/react-native-paper/docs/guides/getting-started
+   */
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel']
+    }
+  }
 };
