e7b427de2078edd81d312d930b9fcc0b34992eeb
Dillon Montag <dillon@thoughtmachine.net>
2024-02-14 14:54:40 +0000

    Additional config for react-native-vector-icons

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,11 @@
 /** @type import("@react-native-community/cli-types").Config */
 module.exports = {
-  project: {
-    ios: {
-      automaticPodsInstallation: true
+  /** Needed for react-native-vector-icons, see https://github.com/oblador/react-native-vector-icons */
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null
+      }
     }
   }
 };
