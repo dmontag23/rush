3a51e596b65ad98ed1b561c5ef56e572e53daabf
Dillon Montag <dillon@thoughtmachine.net>
2024-02-17 12:24:41 +0000

    Wrap app in a query provider

--- App.tsx
+++ App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import {PaperProvider} from 'react-native-paper';
 import {LIGHT_THEME} from './themes';
 import RootNavigator from './components/screens/RootNavigator';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const QUERY_CLIENT = new QueryClient();
 
 const App = () => (
-  /* PaperProvider should be the innermost provider for the app.
-     See https://callstack.github.io/react-native-paper/docs/guides/getting-started */
-  <PaperProvider theme={LIGHT_THEME}>
-    <RootNavigator />
-  </PaperProvider>
+  <QueryClientProvider client={QUERY_CLIENT}>
+    {/* PaperProvider should be the innermost provider for the app. See
+    https://callstack.github.io/react-native-paper/docs/guides/getting-started */}
+    <PaperProvider theme={LIGHT_THEME}>
+      <RootNavigator />
+    </PaperProvider>
+  </QueryClientProvider>
 );
 
 export default App;
