45fe4a1b605f3b638caa3061c5cec5bc0ebfa474
Dillon Montag <dillon@thoughtmachine.net>
2024-02-18 15:44:29 +0000

    Install stack navigator

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/**
- * @format
- */
-
+/* react-native-gesture-handler needs to be the first import
+See https://reactnavigation.org/docs/stack-navigator/ */
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
