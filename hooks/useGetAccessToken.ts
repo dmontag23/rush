a1dd7ce9b8834e8a565dafe8bc47b7475b861a42
Dillon Montag <dillon@thoughtmachine.net>
2024-02-18 11:29:18 +0000

    Add hooks to get and store access tokens

---
+++ hooks/useGetAccessToken.ts
@@ -0,0 +1,12 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useQuery} from '@tanstack/react-query';
+
+const getAccessToken = async () => await AsyncStorage.getItem('access-token');
+
+const useGetAccessToken = () =>
+  useQuery({
+    queryKey: ['accessToken'],
+    queryFn: getAccessToken
+  });
+
+export default useGetAccessToken;
---
+++ hooks/useStoreAuthTokens.ts
@@ -0,0 +1,32 @@
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storeTokens = async (tokens: {
+  accessToken: string;
+  refreshToken: string;
+  ttl: number;
+}) => {
+  // TODO: Use secure storage for tokens instead
+  await AsyncStorage.multiSet([
+    ['access-token', tokens.accessToken],
+    ['refresh-token', tokens.refreshToken],
+    ['token-ttl', tokens.ttl.toString()]
+  ]);
+  return tokens;
+};
+
+const useStoreAuthTokens = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: storeTokens,
+    // Always refetch the access token after success or error
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['accessToken']
+      });
+    }
+  });
+};
+
+export default useStoreAuthTokens;
