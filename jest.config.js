5a4fa32b5a9b950aa15e69e673fe693db54c01dd
Dillon Montag <dillon@thoughtmachine.net>
2024-02-19 09:00:13 +0000

    Add additional setup for jest tests

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/tests/integration/setup.ts'],
   transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|@react-navigation/elements|rn-flipper-async-storage-advanced|react-native-flipper)/)'
+    'node_modules/(?!(@react-native|react-native|@react-navigation/elements|rn-flipper-async-storage-advanced|react-native-flipper|react-native-vector-icons)/)'
   ]
 };
