1e1ea4480081c1f70ac6aff4e2efa05ca7024854
Dillon Montag <dillon@thoughtmachine.net>
2024-02-21 09:53:11 +0000

    Setup lint coverage exceptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   root: true,
-  extends: ['@react-native', 'plugin:prettier/recommended']
+  extends: ['@react-native', 'plugin:prettier/recommended'],
+  ignorePatterns: ['coverage', 'mock-server/dist']
 };
