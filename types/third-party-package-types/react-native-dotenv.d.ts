87fbd2e6ef8a7b7b988051a3e1423950eb88cd4c
Dillon Montag <dillon@thoughtmachine.net>
2024-02-21 14:40:03 +0000

    Fix env var types

--- types/third-party-package-types/react-native-dotenv.d.ts
+++ types/third-party-package-types/react-native-dotenv.d.ts
@@ -1,4 +1,6 @@
-declare module '@env' {
-  export const TODAY_TIX_API_BASE_URL: string;
-  export const TODAY_TIX_WEB_URL: string;
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    TODAY_TIX_API_BASE_URL: string;
+    TODAY_TIX_WEB_URL: string;
+  }
 }
