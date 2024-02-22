a94a2d164ab03b53dfc8942fe784fb0b241c0a17
Dillon Montag <dillon@thoughtmachine.net>
2024-02-22 12:51:15 +0000

    Remove console log

--- components/screens/authentication/flow.spec.tsx
+++ components/screens/authentication/flow.spec.tsx
@@ -14,7 +14,6 @@ describe('The authentication flow', () => {
 
   it('can navigate back to the previous screen', async () => {
     // setup mock success response
-    console.log('API URL: ', process.env.TODAY_TIX_API_BASE_URL);
     nock(process.env.TODAY_TIX_API_BASE_URL)
       .post('/loginTokens')
       .reply(201, {code: 201, data: {}});
