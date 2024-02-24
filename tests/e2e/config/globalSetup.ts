2e83053be7550d3cbb633381733cd58fef941968
Dillon Montag <dillon@thoughtmachine.net>
2024-02-24 16:24:12 +0000

    Remove dep on mock server (and so on node) for e2e tests

--- tests/e2e/config/globalSetup.ts
+++
@@ -1,7 +0,0 @@
-import {listen} from '../../../mock-server/server';
-
-export default async () => {
-  await require('detox/runners/jest').globalSetup();
-  // if running locally start the mock server
-  if (process.env.NODE_ENV === 'DEV') await listen();
-};
