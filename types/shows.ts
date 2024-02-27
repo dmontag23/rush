dc4fa8fe6ca928513a6054e9ccd4ae0166e4e567
Dillon Montag <dillon@thoughtmachine.net>
2024-02-27 22:15:40 +0000

    Add TodayTix show and showtimes types

---
+++ types/shows.ts
@@ -0,0 +1,178 @@
+export enum TodayTixFieldset {
+  Summary = 'SHOW_SUMMARY'
+}
+
+export enum TodayTixLocation {
+  NewYork = 1,
+  London = 2
+}
+
+export enum AdmissionType {
+  Timed = 'TIMED'
+}
+
+export enum AnchorPosition {
+  Top = 'TOP',
+  Center = 'CENTER'
+}
+
+type Category = {
+  _type: string;
+  name: string;
+  id: number;
+  slug: string;
+};
+
+type FileImageDimensions = {
+  width: number;
+  height: number;
+};
+
+type FileDetails = {
+  image?: FileImageDimensions;
+  size: number;
+};
+
+type File = {
+  fileName: string;
+  details: FileDetails;
+  contentType: string;
+  url: string;
+};
+
+type Image = {
+  dominantColor?: string | null;
+  imageUrl: string;
+  videoUrl?: string;
+  _type: string;
+  id: number;
+  anchorPosition: AnchorPosition | null;
+};
+
+type Images = {
+  productMedia: ProductMedia;
+  posterImageLandscape: unknown | null;
+  heroImage: Media | null;
+  imagesAndVideos: ImageAndVideo[];
+  posterImage: Media;
+  posterImageSquare: Media;
+};
+
+type ImageAndVideo = {
+  altText?: string;
+  anchor?: string;
+  media: Media;
+  entryTitle: string;
+  contentModelType: string;
+  entryId: string;
+  updatedAt: string;
+  videoCoverImage?: Media;
+};
+
+type Media = {
+  file: File;
+  description?: string;
+  title?: string;
+};
+
+type ProductMedia = {
+  headerImage?: Media;
+  imagesAndVideos: ImageAndVideo[];
+  posterImage: Media;
+  posterImageSquare: Media;
+  entryTitle: string;
+  appHeroImage: Media;
+  contentModelType: string;
+  entryId: string;
+  updatedAt: string;
+};
+
+export type TicketPrice = {
+  display: string;
+  currency: string;
+  displayRounded: string;
+  value: number;
+};
+
+export enum ProductType {
+  Show = 'SHOW'
+}
+
+export enum Platform {
+  IOS = 'ios',
+  Android = 'android',
+  Web = 'web'
+}
+
+type Promotion = {
+  _type: string;
+  description: string;
+  voucherRestrictionsMessage: unknown | null;
+  id: number;
+  label: string;
+  title: string;
+  voucherCode: unknown | null;
+  platforms: Platform[];
+};
+
+export enum RewardType {
+  Default = 'DEFAULT'
+}
+
+export type Rewards = {_type: string; maxAmount: number; type: RewardType};
+
+export type TodayTixShow = {
+  admissionType: AdmissionType;
+  areLotteryTicketsAvailable: boolean;
+  areRegularTicketsAvailable: boolean;
+  areRushTicketsAvailable: boolean;
+  areaTags: string[];
+  avgRating: number;
+  carouselMedia: Image[];
+  category: Category;
+  description: string;
+  displayName: string;
+  endDate: string;
+  genreTags: string[];
+  hasPromotion: boolean;
+  heroImageUrl: string;
+  id: number;
+  images: Images;
+  isLotteryActive: boolean;
+  isRushActive: boolean;
+  locationId: TodayTixLocation;
+  locationSeoName: string;
+  lotteryBannerText: string | null;
+  lowPriceForLotteryTickets: TicketPrice | null;
+  lowPriceForRegularTickets: TicketPrice;
+  lowPriceForRushTickets: TicketPrice | null;
+  marketableLocationIds: number[];
+  maxDiscountPercentage: number;
+  name: string;
+  posterImageUrl: string;
+  productType: ProductType;
+  promotion: Promotion | null;
+  ratingCount: number;
+  relatedShows: unknown[];
+  rewards: Rewards;
+  rushBannerText: string | null;
+  savingsMessage: string | null;
+  salesMessage: string;
+  shouldShowPromotionsOnCards: boolean;
+  showId: number;
+  showName: string;
+  slug: string | null;
+  startDate: string;
+  subcategories: Category[];
+  venue: string;
+  venueId: string;
+  venueUrl: string;
+};
+
+export type TodayTixShowsReqQueryParams = {
+  areAccessProgramsActive?: boolean;
+  fieldset?: TodayTixFieldset;
+  limit?: number;
+  location?: TodayTixLocation;
+  offset?: number;
+};
---
+++ types/showtimes.ts
@@ -0,0 +1,101 @@
+import {AdmissionType, Platform, Rewards, TicketPrice} from './shows';
+
+export enum Daypart {
+  Evening = 'EVENING',
+  Matinee = 'MATINEE'
+}
+
+export enum DayOfWeek {
+  Monday = 'MONDAY',
+  Tuesday = 'TUESDAY',
+  Wednesday = 'WEDNESDAY',
+  Thursday = 'THURSDAY',
+  Friday = 'FRIDAY',
+  Saturday = 'SATURDAY',
+  Sunday = 'SUNDAY'
+}
+
+type PriceBand = {
+  _type: string;
+  price: TicketPrice;
+  maxContiguousSeats: number;
+  numAssignedSeatsAvailable: number;
+};
+
+type RegularTicketsInfo = {
+  _type: string;
+  availableUntil: string;
+  availableUntilEpoch: number;
+  availabilityLevel: unknown | null;
+  showtimeFeeText: string | null;
+  discount: number;
+  discountForPDP: number;
+  discountAmountForPDP: number;
+  hasNoBookingFee: boolean;
+  hasPromotion: boolean;
+  lowPrice: TicketPrice;
+  lowPriceForShowtimeSelection: TicketPrice;
+  maxContiguousSeats: number;
+  maxTickets: number;
+  minTickets: number;
+  numAssignedSeatsAvailable: number;
+  priceBands: PriceBand[];
+  promotionId: number | null;
+  promotionLabel: string | null;
+  promotionPlatforms: Platform[] | null;
+  rewards: Rewards;
+  savingsMessage: string | null;
+  seatsioEventKey: string;
+};
+
+type LotteryTicketsInfo = {
+  _type: string;
+  availableAfter: string;
+  availableAfterEpoch: number;
+  availableUntil: string;
+  availableUntilEpoch: number;
+  lotteryBannerText: string;
+  lotteryGroupId: number;
+  lotteryGroupTitle: string;
+  lowPrice: TicketPrice;
+  maxTickets: number;
+  minTickets: number;
+};
+
+type RushTicketsInfo = {
+  _type: string;
+  availableAfter: string;
+  availableAfterEpoch: number;
+  availableUntil: string;
+  availableUntilEpoch: number;
+  lowPrice: TicketPrice;
+  maxContiguousSeats: number;
+  maxTickets: number;
+  minTickets: number;
+  quantityAvailable: number;
+  quantityHeld: number;
+  rushBannerText: string;
+};
+
+export type TodayTixShowtime = {
+  _type: string;
+  admissionType: AdmissionType;
+  datetime: string;
+  datetimeEpoch: number;
+  dayOfWeek: DayOfWeek;
+  daypart: Daypart;
+  endDatetime: string | null;
+  endDatetimeEpoch: number | null;
+  endLocalDate: string | null;
+  endLocalTime: string | null;
+  id: number;
+  localDate: string;
+  localTime: string;
+  lotteryTickets: LotteryTicketsInfo | null;
+  numDaysOut: number;
+  partTwoDatetime: string | null;
+  partTwoDatetimeEpoch: number | null;
+  providerShowtimeId: unknown | null;
+  regularTickets: RegularTicketsInfo | null;
+  rushTickets: RushTicketsInfo | null;
+};
