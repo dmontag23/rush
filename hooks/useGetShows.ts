473ad079514619425e2778822f29de2badd74163
Dillon Montag <dillon@thoughtmachine.net>
2024-02-28 11:07:50 +0000

    Add hooks to get shows and showtimes

---
+++ hooks/useGetShows.ts
@@ -0,0 +1,52 @@
+import {useQuery} from '@tanstack/react-query';
+import {todayTixAPI} from '../axiosConfig';
+import {
+  TodayTixFieldset,
+  TodayTixLocation,
+  TodayTixShow,
+  TodayTixShowsReqQueryParams
+} from '../types/shows';
+import {TodayTixAPIError} from '../types/base';
+
+const getShows = async (
+  areAccessProgramsActive?: boolean,
+  fieldset?: TodayTixFieldset,
+  limit?: number,
+  location?: TodayTixLocation,
+  offset?: number
+) => {
+  const queryParams = [
+    areAccessProgramsActive && 'areAccessProgramsActive=1',
+    fieldset && `fieldset=${fieldset}`,
+    limit && `limit=${limit}`,
+    location && `location=${location}`,
+    offset && `offset=${offset}`
+  ]
+    .filter(param => param)
+    .join('&');
+  return todayTixAPI.get<TodayTixShow[]>(
+    `shows${queryParams ? `?${queryParams}` : ''}`
+  );
+};
+
+const useGetShows = ({
+  areAccessProgramsActive,
+  fieldset,
+  limit,
+  location,
+  offset
+}: TodayTixShowsReqQueryParams = {}) =>
+  useQuery<TodayTixShow[], TodayTixAPIError>({
+    queryKey: [
+      'shows',
+      areAccessProgramsActive,
+      fieldset,
+      limit,
+      location,
+      offset
+    ],
+    queryFn: () =>
+      getShows(areAccessProgramsActive, fieldset, limit, location, offset)
+  });
+
+export default useGetShows;
---
+++ hooks/useGetShowtimesWithRushAvailability.ts
@@ -0,0 +1,28 @@
+import {useQueries} from '@tanstack/react-query';
+import {todayTixAPI} from '../axiosConfig';
+import {TodayTixShowtime} from '../types/showtimes';
+
+const getShowtimesWithRushAvailability = async (showId: number) =>
+  todayTixAPI.get<TodayTixShowtime[]>(
+    `shows/${showId}/showtimes/with_rush_availability`
+  );
+
+type UseGetShowtimesWithRushAvailabilityProps = {
+  showIds: number[];
+};
+
+const useGetShowtimesWithRushAvailability = ({
+  showIds
+}: UseGetShowtimesWithRushAvailabilityProps) =>
+  useQueries({
+    queries: showIds.map(showId => ({
+      queryKey: ['showtimesWithRushAvailability', showId],
+      queryFn: () => getShowtimesWithRushAvailability(showId)
+    })),
+    combine: results => ({
+      data: results.map(result => result.data),
+      isPending: results.some(result => result.isPending)
+    })
+  });
+
+export default useGetShowtimesWithRushAvailability;
