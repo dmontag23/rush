c449c7a396653910a60feab903ac78c033513db8
Dillon Montag <dillon@thoughtmachine.net>
2024-03-03 20:32:58 +0000

    Add e2e test for show details screen

--- tests/e2e/rushShows.test.ts
+++ tests/e2e/rushShows.test.ts
@@ -2,7 +2,7 @@ import {expect} from 'detox';
 import {describe, it, beforeEach} from '@jest/globals';
 import {login} from './utils/utils';
 
-describe('Rush shows page', () => {
+describe('Rush shows', () => {
   beforeEach(login);
 
   it('can see the expected shows', async () => {
@@ -17,5 +17,40 @@ describe('Rush shows page', () => {
 
     await element(by.id('rushShows')).scroll(400, 'down');
     await expect(tinaShowName).toBeVisible();
+  });
+
+  it('can select a show and number of tickets', async () => {
+    // select the matinee show
+    const guysAndDollsText = element(by.text('Guys & Dolls'));
+    await guysAndDollsText.tap();
+    await expect(guysAndDollsText).toBeVisible();
+    const selectATimeText = element(by.text('Select a Time'));
+    await expect(selectATimeText).toBeVisible();
+    const matineeShow = element(by.text('14:00'));
+    await expect(matineeShow).toBeVisible();
+    await expect(element(by.text('19:30'))).toBeVisible();
+    const numberOfTicketText = element(by.text('Number of Tickets'));
+    await expect(numberOfTicketText).not.toBeVisible();
+
+    // select the number of desired tickets
+    await matineeShow.tap();
+    await expect(numberOfTicketText).toBeVisible();
+    const ticketButton = element(by.text('1'));
+    await expect(ticketButton).toBeVisible();
+    await expect(element(by.text('2'))).toBeVisible();
+    await ticketButton.tap();
+
+    // navigate to a different show
+    const backButton = element(by.label('Back button')).atIndex(1);
+    await backButton.tap();
+    await element(by.label('SIX the Musical')).tap();
+    await expect(selectATimeText).toBeVisible();
+    await expect(element(by.text('19:00'))).toBeVisible();
+    await expect(numberOfTicketText).not.toBeVisible();
+
+    // navigate back to the original show and ensure the original selection remains
+    await backButton.tap();
+    await guysAndDollsText.tap();
+    await expect(numberOfTicketText).toBeVisible();
   });
 });
