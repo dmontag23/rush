e4199d76bed4e556c441e871722d7e5720220133
Dillon Montag <dillon@thoughtmachine.net>
2024-03-16 17:23:33 +0000

    Add customer type

---
+++ types/customer.ts
@@ -0,0 +1,43 @@
+import {Money} from "./shows";
+
+enum MarketingEmailConsent {
+  Granted = "GRANTED"
+}
+
+enum PrivacyLaw {
+  None = "NONE"
+}
+
+type RewardsBalance = {
+  _type: string;
+  balance: Money;
+  pendingBalance: Money | null;
+  expirationDate: string;
+};
+
+export type TodayTixCustomer = {
+  _type: string;
+  id: string;
+  appleId: unknown | null;
+  googleId: string;
+  confirmedEmailAddresses: string[];
+  countryCode: number;
+  creditBalance: Money;
+  defaultPaymentMethod: unknown | null;
+  email: string;
+  facebookId: unknown | null;
+  firstName: string;
+  lastName: string;
+  marketingEmailConsent: MarketingEmailConsent.Granted;
+  phone: string;
+  privacyLaw: PrivacyLaw.None;
+  regionCode: string;
+  rewardsBalance: RewardsBalance;
+  v1Token: string;
+  canReceiveSms: boolean;
+  traits: {
+    recently_viewed_v2: string;
+    recently_viewed: string[];
+  };
+  homeLocationId: number;
+};
