5ce0ea5fcf9bf3edc19b53a1cbec67fbd68793e3
Dillon Montag <dillon@thoughtmachine.net>
2024-03-16 13:02:56 +0000

    Improve fake timer setup

--- tests/integration/setup.ts
+++ tests/integration/setup.ts
@@ -3,29 +3,32 @@ See https://reactnavigation.org/docs/testing/ */
 
 /* The following import and mock are needed for AsyncStorage.
 See https://react-native-async-storage.github.io/async-storage/docs/advanced/jest */
-import {afterEach, beforeAll, beforeEach, jest} from "@jest/globals";
+import {afterEach, beforeEach, jest} from "@jest/globals";
 import MockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
 import "@testing-library/react-native/extend-expect";
 import nock from "nock";
 import "react-native-gesture-handler/jestSetup";
+import {act} from "testing-library/extension";
 
 jest.mock("@react-native-async-storage/async-storage", () => MockAsyncStorage);
 
 export const systemTime = new Date(2021, 4, 23);
 
-beforeAll(() => {
-  /* useFakeTimers is needed for animated components
+beforeEach(() => {
+  /* useFakeTimers is needed for animated components and to mock the system time
   see https://github.com/jestjs/jest/issues/6434
   the doNotFake option is due to an open issue with nock/react testing library
   see https://github.com/nock/nock/issues/2200 */
-  jest.useFakeTimers({doNotFake: ["nextTick", "setImmediate"]});
-});
-
-beforeEach(() => {
-  jest.setSystemTime(systemTime);
+  jest
+    .useFakeTimers({
+      doNotFake: ["nextTick", "setImmediate"]
+    })
+    .setSystemTime(systemTime);
 });
 
 afterEach(() => {
+  act(jest.runOnlyPendingTimers);
+  jest.useRealTimers();
   nock.abortPendingRequests();
   nock.cleanAll();
   MockAsyncStorage.clear();
