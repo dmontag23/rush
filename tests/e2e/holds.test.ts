f8634c87919cc9500071fbcb03ba1fa697ef110e
Dillon Montag <dillon@thoughtmachine.net>
2024-03-16 23:07:33 +0000

    Add holds e2e test

---
+++ tests/e2e/holds.test.ts
@@ -0,0 +1,18 @@
+import {beforeEach, describe, it} from "@jest/globals";
+import {expect} from "detox";
+
+import {login} from "./utils/utils";
+
+describe("Holds", () => {
+  beforeEach(login);
+
+  it("can place a hold for a show where tickets are already open", async () => {
+    // select a showtime that is already open
+    await element(by.text("Guys & Dolls")).tap();
+    await element(by.text("19:30")).tap();
+    await element(by.text("1")).tap();
+    await expect(element(by.text("You've won 1 ticket(s) to"))).toBeVisible();
+
+    // TODO: Expand this test once the hold confirmation page is more fully built out
+  });
+});
