9802e3064a7df54add8904f792ed29588382d499
Dillon Montag <dillon@thoughtmachine.net>
2024-03-19 22:22:46 +0000

    Try clearing Netlify blobs in e2e test setup

--- tests/e2e/utils/setup.ts
+++ tests/e2e/utils/setup.ts
@@ -1,8 +1,27 @@
 import {beforeEach} from "@jest/globals";
+import {getStore} from "@netlify/blobs";
 
 beforeEach(async () => {
   // this is necessary in order to clear async storage before each test
   await device.uninstallApp();
   await device.installApp();
   await device.launchApp();
+
+  // delete all data from Netlify
+  try {
+    const rushGrantsStore = getStore({
+      name: "rush-grants",
+      siteID: process.env.NETLIFY_SITE_ID,
+      token: process.env.NETLIFY_API_KEY
+    });
+
+    const {blobs: rushGrantsInStore} = await rushGrantsStore.list();
+    await Promise.all(
+      rushGrantsInStore.map(
+        async grant => await rushGrantsStore.delete(grant.key)
+      )
+    );
+  } catch (error: unknown) {
+    console.log(`There was an error deleting data from Netlify: ${error}`);
+  }
 });
