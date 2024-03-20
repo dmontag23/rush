bb29acf9ba8a9830e1cb4d33eb35c714123323f4
Dillon Montag <dillon@thoughtmachine.net>
2024-03-20 21:21:21 +0000

    Move Netlify helper function to utils

--- tests/e2e/utils/utils.ts
+++ tests/e2e/utils/utils.ts
@@ -1,3 +1,5 @@
+import {getStore} from "@netlify/blobs";
+
 /* TODO: Maybe find a better way to do this? Testing the login functionality
 for every test isn't great. Something like a deep link may work better, but
 has other disadvantages. Perhaps exposing a way to do this from the mock server somehow
@@ -12,4 +14,29 @@ export const login = async () => {
     .atIndex(1)
     .typeText("refresh-token");
   await element(by.text("Login")).tap();
+};
+
+const clearAllRushGrants = async () => {
+  try {
+    const rushGrantsStore = getStore({
+      name: "rush-grants",
+      siteID: process.env.NETLIFY_SITE_ID,
+      token: process.env.NETLIFY_API_KEY
+    });
+
+    const {blobs: rushGrantsInStore} = await rushGrantsStore.list();
+    await Promise.all(
+      rushGrantsInStore.map(
+        async grant => await rushGrantsStore.delete(grant.key)
+      )
+    );
+  } catch (error: unknown) {
+    console.log(
+      `There was an error clearing rush grants from Netlify: ${error}`
+    );
+  }
+};
+
+export const clearNetlifyData = async () => {
+  await clearAllRushGrants();
 };
