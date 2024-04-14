bf9330d50914c3c60acf7c8e3b996112b9ee3771
dmontag23 <dmontag23@gmail.com>
2024-04-14 10:47:35 +0100

    Remove the TodayTix web url

--- types/third-party-package-types/react-native-dotenv.d.ts
+++ types/third-party-package-types/react-native-dotenv.d.ts
@@ -3,6 +3,5 @@ declare namespace NodeJS {
     TODAY_TIX_API_BASE_URL: string;
     TODAY_TIX_API_OAUTH_ENDPOINT: string;
     TODAY_TIX_API_V2_ENDPOINT: string;
-    TODAY_TIX_WEB_URL: string;
   }
 }
